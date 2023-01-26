836ce067ca4ed956ed6c4f8161fb63d16d593811
grecheskiy <grecheskiy@rambler.ru>
2023-01-27 00:33:48 +0300

    add homework_3_5

---
+++ homework_3_5/Program.cs
@@ -0,0 +1,9 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+int [] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int n = array.Length;
+int i = 0;
+int p = 1;
+while (i < n)
+{p = p * array[i];
+i++;}
+Console.WriteLine(p);
