f49bed6fa6354b8f32e98bfbfa19c87aeae7c57b
grecheskiy <grecheskiy@rambler.ru>
2023-01-26 23:58:11 +0300

    add homework_3_4

---
+++ homework_3_4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Подсчитать сумму цифр в числе
+Console.WriteLine("Enter three-digit number: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int x1 = n / 100;
+int x2 = (n / 10) % 10;
+int x3 = n % 10;
+int s = x1 + x2 + x3;
+if (n>=100)
+Console.WriteLine(s);
+else
+Console.WriteLine(" False ");
