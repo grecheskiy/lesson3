81a4b5c8f9182e37a06d638c1e2314124298db2f
grecheskiy <grecheskiy@rambler.ru>
2023-01-26 23:35:01 +0300

    add homework_3_3

---
+++ homework_3_3/Program.cs
@@ -0,0 +1,8 @@
+﻿//Возведите число А в натуральную степень B используя цикл
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+int n = array.Length;
+int i = 0;
+while (i < n)
+{double result = Math.Pow(array[i],array[i]);
+{Console.WriteLine(result);}
+i++;}
