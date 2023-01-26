38037eacb570db0558c93418686a2c9e774829f1
grecheskiy <grecheskiy@rambler.ru>
2023-01-26 23:07:25 +0300

    add homework_3_1

---
+++ homework_3_1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Найти кубы чисел от 1 до N
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int n = array.Length;
+int i = 0;
+while (i < n)
+{int k = array[i] * array[i];
+{Console.WriteLine(k);}
+i++;}
