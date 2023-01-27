580b1fec208bacd71d383c47658cd42ae5dcad20
grecheskiy <grecheskiy@rambler.ru>
2023-01-27 23:19:36 +0300

    add homework_3_6

---
+++ homework_3_6/Program.cs
@@ -0,0 +1,7 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int k = 0;
+for (int i = 0; i < array.Length; i++)
+     if ((array[i] % 2) == 0)
+          {k = array[i] * array[i] * array[i];
+           Console.WriteLine(k);}
