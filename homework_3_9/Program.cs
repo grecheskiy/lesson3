2679d6e8a60375016339dff0d62daf0ba915298e
grecheskiy <grecheskiy@rambler.ru>
2023-01-28 14:35:40 +0300

    add  homework_3_9

---
+++ homework_3_9/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задать массив, 
+//заполнить случайными положительными трёхзначными числами. 
+//Показать количество нечетных\четных чисел
+int[] arr = new int[10];
+Random rnd = new Random();
+int count1 = 0; int count2 = 0;
+for (int i=0; i<arr.Length; i++)
+{arr[i] = rnd.Next(100,999);
+Console.Write($"{arr[i]} ");}
+Console.WriteLine();
+for (int j=0; j<arr.Length; j++)
+{if( arr[j] % 2 != 0 )
+ count1 = count1 + 1;
+ else count2 = count2 +1;}
+ Console.Write($"number of odd numbers: {count1}");
+ Console.WriteLine();
+ Console.Write($"number of even numbers: {count2}");
+ Console.WriteLine();
