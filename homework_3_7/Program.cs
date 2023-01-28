3687c616d74b9e30e396181345ba5b9ab2daa5e7
grecheskiy <grecheskiy@rambler.ru>
2023-01-28 13:57:53 +0300

    add  homework_3_7

---
+++ homework_3_7/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задать массив из 8 элементов 
+//заполненных нулями и единицами 
+//и вывести их на экран
+int[] arr = new int[8];
+Random rnd = new Random();
+for (int i=0; i<arr.Length; i++)
+{arr[i] = rnd.Next(0,2);
+Console.Write($"{arr[i]} ");}
+Console.WriteLine();
