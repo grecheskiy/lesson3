77323c69dd21355532a1a4cf09a62e9fdc012fe0
grecheskiy <grecheskiy@rambler.ru>
2023-01-28 22:14:45 +0300

    add homework_3_11

---
+++ homework_3_11/Program.cs
@@ -0,0 +1,11 @@
+﻿//Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int[] arr = new int[10];
+Random rnd = new Random();
+int sum = 0;
+for (int i=0; i<arr.Length; i++)
+{arr[i] = rnd.Next(1,10);
+Console.Write($"{arr[i]} ");}
+Console.WriteLine();
+for (int j=0; j<arr.Length - 1; j+=2)
+{sum = sum + arr[j];}
+Console.WriteLine($"Сумма чисел на нечетных позициях : {sum}");
