7bf71b6bc3ca29fcb19012a32ecc666b5801297e
grecheskiy <grecheskiy@rambler.ru>
2023-01-28 14:00:32 +0300

    add  homework_3_8

---
+++ homework_3_8/Program.cs
@@ -0,0 +1,10 @@
+﻿//Определить, присутствует ли в заданном массиве, некоторое число
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+Console.WriteLine("Enter find number: ");
+int x = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 0; i < array.Length; i++)
+   {  if (array[i] == x) 
+          {Console.WriteLine("Yes");
+          break;}
+       else {Console.WriteLine("No");}   
+   }  
